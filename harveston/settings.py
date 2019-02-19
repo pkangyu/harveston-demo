1c5a518215846a125d247b8545089e83f9c73a62
pkangyu <pohkangyu@gmail.com>
2019-02-19 12:21:17 +0800

    changed settings

--- harveston/settings.py
+++ harveston/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +28,7 @@ SECRET_KEY = '!_viducnf+7o8_!r90%8+@hnqfop+_d%+sl)d1m(b#803hn5+_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['harveston-query.herokuapp.com']
 
 # Application definition
 
